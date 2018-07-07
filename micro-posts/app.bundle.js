471901a93b9b199c921dd4cfdf6d331e43b2bf38
Bulian1311 <bulian1311@gmail.com>
2018-07-07 15:34:09 +0300

    done

--- micro-posts/app.bundle.js
+++ micro-posts/app.bundle.js
@@ -9182,7 +9182,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
 
-var http = new _http2.default('http://reduxblog.herokuapp.com/api/');
+var http = new _http2.default('https://reduxblog.herokuapp.com/api/');
 var ui = new _ui2.default();
 
 document.addEventListener('DOMContentLoaded', getPosts);
